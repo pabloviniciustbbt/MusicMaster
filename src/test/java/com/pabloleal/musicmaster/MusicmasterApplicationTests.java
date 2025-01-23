5efbb36faf2e6e0df548265db52c7eea37572659
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2025-01-23 07:00:03 -0300

    Commit Inicial do Projeto MusicMaste

---
+++ src/test/java/com/pabloleal/musicmaster/MusicmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pabloleal.musicmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
