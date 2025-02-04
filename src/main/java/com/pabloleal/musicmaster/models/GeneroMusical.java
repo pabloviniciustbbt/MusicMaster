ead629fe53f3072c1dd459413e12fb8a9be034e8
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2025-02-04 07:48:55 -0300

    feat: adiciona enum GeneroMusical para categorização de músicas

---
+++ src/main/java/com/pabloleal/musicmaster/models/GeneroMusical.java
@@ -0,0 +1,14 @@
+package com.pabloleal.musicmaster.models;
+
+public enum GeneroMusical {
+
+    POP,
+    ROCK,
+    ELETRONICA,
+    REGGAE,
+    FUNK,
+    SAMBA,
+    HIPHOP,
+    TRAP,
+    CLASSICA;
+}
