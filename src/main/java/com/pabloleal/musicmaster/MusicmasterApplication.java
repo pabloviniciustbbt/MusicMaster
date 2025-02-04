5ad0ae059871252c818da0ab0dce84ec632324d3
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2025-02-04 13:58:11 -0300

    refactor: implementa CommandLineRunner e injeta repositórios em MusicmasterApplication

--- src/main/java/com/pabloleal/musicmaster/MusicmasterApplication.java
+++ src/main/java/com/pabloleal/musicmaster/MusicmasterApplication.java
@@ -1,13 +1,30 @@
 package com.pabloleal.musicmaster;
 
+import com.pabloleal.musicmaster.main.Main;
+import com.pabloleal.musicmaster.repository.ArtistaRepository;
+import com.pabloleal.musicmaster.repository.MusicaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MusicmasterApplication {
+public class MusicmasterApplication implements CommandLineRunner {
+
+	@Autowired
+	private ArtistaRepository artistaRepository;
+	@Autowired
+	private MusicaRepository musicaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MusicmasterApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+
+		Main main = new Main(artistaRepository, musicaRepository);
+		main.exibeMenu();
+
+	}
 }
