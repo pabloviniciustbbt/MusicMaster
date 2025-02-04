be23f7aa87ef7ce0822e52275551f3b01a801a4b
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2025-02-04 16:37:15 -0300

    feat: adiciona método findByNomeContainingIgnoreCase em ArtistaRepository

--- src/main/java/com/pabloleal/musicmaster/repository/ArtistaRepository.java
+++ src/main/java/com/pabloleal/musicmaster/repository/ArtistaRepository.java
@@ -3,5 +3,8 @@ package com.pabloleal.musicmaster.repository;
 import com.pabloleal.musicmaster.models.Artista;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ArtistaRepository extends JpaRepository<Artista, Long> {
+    Optional<Artista> findByNomeContainingIgnoreCase(String nomeDigitado);
 }
