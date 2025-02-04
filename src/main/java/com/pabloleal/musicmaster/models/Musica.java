71752cf99dc862e848e27a2198bc62b17759d802
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2025-02-04 07:49:17 -0300

    feat: adiciona modelo Musica com relacionamento a Artista e GeneroMusical

---
+++ src/main/java/com/pabloleal/musicmaster/models/Musica.java
@@ -0,0 +1,57 @@
+package com.pabloleal.musicmaster.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table (name = "musicas")
+public class Musica {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private GeneroMusical generoMusical;
+
+    @ManyToOne
+    private Artista artista;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public GeneroMusical getGeneroMusical() {
+        return generoMusical;
+    }
+
+    public void setGeneroMusical(GeneroMusical generoMusical) {
+        this.generoMusical = generoMusical;
+    }
+
+    public Artista getArtista() {
+        return artista;
+    }
+
+    public void setArtista(Artista artista) {
+        this.artista = artista;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome da Música = " + titulo +
+               "\nGenero Musical = " + generoMusical +
+               "\nArtista = " + artista;
+    }
+}
