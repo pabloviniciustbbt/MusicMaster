e4bd2b18b9ea80edd6f2df6f3a1bc641e010fd92
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2025-02-04 07:47:41 -0300

    feat: adiciona modelo Artista com atributos básicos

---
+++ src/main/java/com/pabloleal/musicmaster/models/Artista.java
@@ -0,0 +1,49 @@
+package com.pabloleal.musicmaster.models;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "artistas")
+public class Artista {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (unique = true)
+    private String nome;
+
+    @OneToMany (mappedBy = "artista")
+    private List<Musica> musicas;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Musica> getMusicas() {
+        return musicas;
+    }
+
+    public void setMusicas(List<Musica> musicas) {
+        this.musicas = musicas;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+}
