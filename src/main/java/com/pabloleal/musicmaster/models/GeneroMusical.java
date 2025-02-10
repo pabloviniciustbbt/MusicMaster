62215f0d3343a6f8d2fa46c30a484f8bea69b5f8
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2025-02-10 11:05:42 -0300

    feat: adiciona identificadores numéricos e método de conversão em GeneroMusical

--- src/main/java/com/pabloleal/musicmaster/models/GeneroMusical.java
+++ src/main/java/com/pabloleal/musicmaster/models/GeneroMusical.java
@@ -1,14 +1,43 @@
 package com.pabloleal.musicmaster.models;
 
 public enum GeneroMusical {
+    AXE(1),
+    BLUES(2),
+    CLASSICA(3),
+    ELETRONICA(4),
+    FORRO(5),
+    FUNK(6),
+    GOSPEL(7),
+    HIPHOP(8),
+    INDIE(9),
+    MPB(10),
+    PAGODE(11),
+    POP(12),
+    PUNK(13),
+    REGGAE(14),
+    ROCK(15),
+    SAMBA(16),
+    SERTANEJO(17),
+    SOUNDTRACKS(18),
+    TRAP(19);
 
-    POP,
-    ROCK,
-    ELETRONICA,
-    REGGAE,
-    FUNK,
-    SAMBA,
-    HIPHOP,
-    TRAP,
-    CLASSICA;
+    private int numeroGenero;
+
+    GeneroMusical(int numeroGenero) {
+        this.numeroGenero = numeroGenero;
+    }
+
+    public int getNumeroGenero() {
+        return numeroGenero;
+    }
+
+    public static GeneroMusical fromInt(int i){
+        for (GeneroMusical generoMusical : GeneroMusical.values()){
+            if (generoMusical.getNumeroGenero() == i){
+                return generoMusical;
+            }
+        }
+
+        throw new IllegalArgumentException("Nenhum categoria encontrada para a string fornecida: " + i);
+    }
 }
