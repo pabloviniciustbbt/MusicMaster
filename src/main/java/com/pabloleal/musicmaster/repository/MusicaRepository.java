3c70177f6dd56e82c2bed4ca0cbda5a0b17eb256
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2025-02-11 16:00:12 -0300

    feat: adiciona consulta personalizada para buscar músicas por artista

--- src/main/java/com/pabloleal/musicmaster/repository/MusicaRepository.java
+++ src/main/java/com/pabloleal/musicmaster/repository/MusicaRepository.java
@@ -2,6 +2,12 @@ package com.pabloleal.musicmaster.repository;
 
 import com.pabloleal.musicmaster.models.Musica;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MusicaRepository extends JpaRepository<Musica, Long> {
+    @Query("SELECT m FROM Musica m JOIN m.artista a WHERE a.nome ILIKE %:nomeDigitado%")
+    List<Musica> buscaMusicasPorArtista(String nomeDigitado);
+
 }
