c1d37155b7a38a932741d02a23910e3465c1507d
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2025-02-13 07:35:11 -0300

    feat: adicionar método para listar artistas em ordem alfabética

--- src/main/java/com/pabloleal/musicmaster/repository/ArtistaRepository.java
+++ src/main/java/com/pabloleal/musicmaster/repository/ArtistaRepository.java
@@ -2,9 +2,14 @@ package com.pabloleal.musicmaster.repository;
 
 import com.pabloleal.musicmaster.models.Artista;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ArtistaRepository extends JpaRepository<Artista, Long> {
     Optional<Artista> findByNomeContainingIgnoreCase(String nomeDigitado);
+
+    @Query("SELECT a.nome FROM Artista a ORDER BY a.nome ASC")
+    List<String> listarArtistasOrdemAlfabetica();
 }
